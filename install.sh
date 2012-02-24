02abc12c02f7af9b7d46d2381ea9a739d100d4bb
Jason Young <jason.young@qualcomm.com>
2012-02-24 10:33:39 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# TODO
+# only tested on linux
+# When to use .bashrc vs .bash_profile?
+
+PWD=`pwd`
+
+# install vim stuff
+if [ ! -e $HOME/.vim ]; then
+	ln -s $PWD/vim $HOME/.vim
+else
+	echo "$HOME/.vim already exists, skipping"
+fi
+
+if [ ! -e $HOME/.vimrc ]; then
+	ln -s $PWD/vim/dotvimrc $HOME/.vimrc
+else
+	echo "$HOME/.vimrc already exists, skipping"
+fi
+
+# install bash stuff
+if [ ! -e $HOME/.bashrc ]; then
+	ln -s $PWD/bash/dotbashrc $HOME/.bashrc
+else
+	echo "$HOME/.bashrc already exists, skipping"
+fi
+
+
